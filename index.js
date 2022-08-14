484ad6e9f9676f182bc38e5159a1d5c272b7934a
m.shibuta <m.shibuta@shibutakitokunoMacBook-Air.local>
2022-08-14 15:05:59 +0900

    feat: Authorizationのサンプル

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 import express from 'express'
 const app = express()
+
+const isAuthorized = (req, res, next) => {
+  const auth = req.headers.authorization;
+  console.log(auth);
+  if (auth === 'secretpassword') {
+    next()
+  } else {
+    res.status(401);
+    res.send('Not permitted');
+  }
+}
+
 const port = 3000
 
 
 app.use((req, res, next) => {
-  console.log("middleware before request")
-  next()
+  console.log("middleware before request");
+  next();
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Hello World!')
+  next();
 })
 
-app.get("/users", (req, res) => {
+app.get("/users", isAuthorized, (req, res, next) => {
   res.json([
     {
       id: 1,
       name: "User Userson",
     },
   ]);
+  next();
 });
 
-app.get("/products", (req, res) => {
+app.get("/products", (req, res, next) => {
   res.json([
     {
       id: 1,
       name: "The Bluest Eye",
     },
   ]);
+  next();
 });
 
 app.use((req, res, next) => {
-  console.log("middleware before after")
-  next()
+  console.log("middleware before after");
+  next();
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
