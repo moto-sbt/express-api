8e61095e515d875ef7f76a05a9c366d27936abe4
moto-sbt <moto1220.sbt@outlook.jp>
2022-08-14 11:26:02 +0900

    middlewareが動くとこまで

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
+
+app.use((req, res, next) => {
+  console.log("middleware before request")
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get("/products", (req,res) => {
+  const products = [
+  {
+    id: 1,
+    name: "taro",
+  },
+  {
+    id: 2,
+    name: "jiro",
+  },
+  {
+    id: 3,
+    name: "saburo",
+  },
+ ];
+
+ res.json(products);
+});
+
+app.use((req, res, next) => {
+  console.log("middleware before after")
+  next()
 })
 
 app.listen(port, () => {
