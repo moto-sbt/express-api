f10d961a999387a6deea7dc666162c6fe7d7d683
moto-sbt <moto1220.sbt@outlook.jp>
2022-08-23 10:35:29 +0900

    docker からデータをとれるようにした(#9)

---
+++ src/service/userService.ts
@@ -0,0 +1,11 @@
+import { AppDataSource } from '../app-data-source';
+import { User } from '../entity/User';
+
+export class UserService {
+  
+  private userRepository = AppDataSource.getRepository(User)
+
+  async all() {
+    return this.userRepository.find();
+  }
+}
