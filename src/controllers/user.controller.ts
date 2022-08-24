79289eb4922f4fc98d526deec50e45db91a3ef5b
moto-sbt <moto1220.sbt@outlook.jp>
2022-08-24 16:29:27 +0900

    refactor: ちょこっと修正

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,13 +2,12 @@ import { Controller, Get, UseBefore } from 'routing-controllers';
 import { AuthMiddleware } from '../middlewares/auth.middleware';
 import { UserService } from '../services/userService';
 
-const userService = new UserService();
-
 @Controller('/users')
 @UseBefore(AuthMiddleware)
 export class UserController {
   @Get('/')
   async getAll() {
+    const userService = new UserService();
     const users = await userService.all()
     return users;
   }
